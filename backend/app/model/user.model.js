d0d00b9e140d8d04bf0a30175f8c5ab58d1a8d9a
bhupendrabanothe <bhupendrabanothe@gmail.com>
2021-03-12 20:39:13 +0530

    chore: database config

---
+++ backend/app/model/user.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255,
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = mongoose.model('user', userSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,21 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const connectDB = require("./app/config/database");
+const bodyParser = require('body-parser');
+const dotenv = require("dotenv");
+
 const port = 5000
+
+
+const app = express();
+dotenv.config();
+connectDB();
+
+app.use(bodyParser.urlencoded({ extended: true })) // parse requests of content-type - application/x-www-form-urlencoded
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.json()); // for body parser
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
