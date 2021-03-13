6de65545d222bf2951ad9e599eec08fd5df9a2b3
omjdeshmukh <omjdeshmukh@gmail.com>
2021-03-14 04:30:38 +0530

    service task completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,11 @@
-<<<<<<< HEAD
-import React from "react";
-// import { Counter } from "./features/counter/Counter";
-import "./App.css";
-import Navbar from "./Components/Header/Navbar";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import MostVisited from './Components/MostVisited/MostVisited';
-
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <MostVisited/>
-      </div>
-    </Router>
-  );
-}
-
-export default App;
-=======
 import React from "react";
 import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import Navbar from "./Components/Header/Navbar";
 import Main from "./Components/Tagline/Main";
+import Services from './Components/Services/Services';
+import CarouselBar from './Components/OwlCarousel/CarouselBar'
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -34,10 +16,11 @@ function App() {
         <Switch>
           <Main />
         </Switch>
+        <Services />
+        <CarouselBar/>
       </div>
     </Router>
   );
 }
 
 export default App;
->>>>>>> 9c69f03164ac2f5710cec63851394c10416ca4bd
---
+++ frontend/src/Components/Services/CardData.js
@@ -0,0 +1,45 @@
+const CardData = [
+  {
+    imgsrc: "https://source.unsplash.com/random",
+    title: "Medical",
+    info: "this is the field of service information",
+  },
+  {
+    imgsrc: "https://source.unsplash.com/random",
+    title: "Medical",
+    info: "this is the field of service information",
+  },
+  {
+    imgsrc: "https://source.unsplash.com/random",
+    title: "Medical",
+    info: "this is the field of service information",
+  },
+  {
+    imgsrc: "https://source.unsplash.com/random",
+    title: "Medical",
+    info: "this is the field of service information",
+  },
+  {
+    imgsrc: "https://source.unsplash.com/random",
+    title: "Medical",
+    info: "this is the field of service information",
+  },
+  {
+    imgsrc: "https://source.unsplash.com/random",
+    title: "Medical",
+    info: "this is the field of service information",
+  },
+  {
+    imgsrc: "https://source.unsplash.com/random",
+    title: "Medical",
+    info: "this is the field of service information",
+  },
+  {
+    imgsrc: "https://source.unsplash.com/random",
+    title: "Medical",
+    info: "this is the field of service information",
+  },
+];
+
+export default CardData;
+
