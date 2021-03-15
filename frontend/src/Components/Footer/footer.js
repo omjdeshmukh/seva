f8b7a2cafc82a7e1eb46b44eafc692ebb89b3a88
SarkarPriyanshu <provashpriyanshu@gmail.com>
2021-03-15 11:33:53 +0530

    updated footer to homepage

--- frontend/src/Components/Footer/footer.js
+++ frontend/src/Components/Footer/footer.js
@@ -1,134 +1,187 @@
 import React from "react";
 import styled from "styled-components";
-import FacebookIcon from "@material-ui/icons/Facebook";
-import InsertCommentIcon from "@material-ui/icons/InsertComment";
-import LanguageIcon from "@material-ui/icons/Language";
-import HeadsetIcon from "@material-ui/icons/Headset";
-import EmailIcon from "@material-ui/icons/Email";
-import LocationOnIcon from "@material-ui/icons/LocationOn";
-import GitHubIcon from "@material-ui/icons/GitHub";
-import Appstore from "./images/appstore.png";
-import Playstore from "./images/appstore.png";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div>
-      <Container>
-        <First>
-          <h1>Seva</h1>
+    <FooterContainer>
+      <FooterInnerContainer>
+        <FooterAboutUs>
+          <h3>SEVA</h3>
           <p>
-            Consectetur adipisicing elit sed do eiusmod tempor incididunt labore
-            etolore magna aliqua enim adena minim veniam quistane nostrud catate
-            ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, animi
+            fuga? Commodi magnam nam nostrum soluta laudantium quas impedit eum
+            placeat error?
           </p>
-          <FirstContainer>
-            <FacebookIcon style={{ width: 50 }} />
-            <InsertCommentIcon style={{ width: 50 }} />
-            <LanguageIcon style={{ width: 50 }} />
-            <GitHubIcon style={{ width: 50 }} />
-          </FirstContainer>
-        </First>
-        <Second>
-          <SecondContainer>
-            <Image>
-              <HeadsetIcon style={{ color: "yellow" }} />
-            </Image>
-            <Column>
-              <span>24/7 Support:</span>
-              <span>0800 12345 678</span>
-            </Column>
-          </SecondContainer>
-          <SecondContainer>
-            <Image>
-              {" "}
-              <EmailIcon style={{ color: "yellow" }} />
-            </Image>
-            <Column>
-              <span>Say Hello To Us</span>
-              <span>Click Here to Contact Us</span>
-            </Column>
-          </SecondContainer>
-          <SecondContainer>
-            <Image>
-              <LocationOnIcon style={{ color: "yellow" }} />
-            </Image>
-            <Column>
-              <span>Our Active Location</span>
-              <span>167 Airline Suite, 456GA3, UK</span>
-            </Column>
-          </SecondContainer>
-        </Second>
-        <Third>
-          <h4>Download Mobile App</h4>
-          <App>
-            <img src={Appstore} alt="Appstore" />
-            <img src={Playstore} alt="playstore" />
-          </App>
-        </Third>
-      </Container>
-      <Copyrights>
-        <p>Copyrights © 2021. All Rights Reserved by Seva.</p>
-      </Copyrights>
-    </div>
+          <SocialLinks>
+            <Link to="#">
+              <li>
+                <img src="/Assets/Icons/Facebookicon.png" alt="Facebook icon" />
+              </li>
+            </Link>
+            <Link to="#">
+              <li>
+                <img
+                  src="/Assets/Icons/Intagramicon.png"
+                  alt="Instagram icon"
+                />
+              </li>
+            </Link>
+            <Link to="#">
+              <li>
+                <img src="/Assets/Icons/Googleicon.png" alt="Linkden icon" />
+              </li>
+            </Link>
+            <Link to="#">
+              <li>
+                <img src="/Assets/Icons/Githubicon.png" alt="Linkden icon" />
+              </li>
+            </Link>
+          </SocialLinks>
+        </FooterAboutUs>
+        <FooterServices>
+          <FooterServiceDiv>
+            <ServiceIcon>
+              <img src="./Assets/Icons/Caresupporticon.png" />
+            </ServiceIcon>
+            <ServiceInfo>
+              <h4>24/7 Support:</h4>
+              <p>1234 123 123</p>
+            </ServiceInfo>
+          </FooterServiceDiv>
+          <FooterServiceDiv>
+            <ServiceIcon>
+              <img src="./Assets/Icons/Messageicon.png" />
+            </ServiceIcon>
+            <ServiceInfo>
+              <h4>Say Hello To Us</h4>
+              <p>Click Here to Contact Us</p>
+            </ServiceInfo>
+          </FooterServiceDiv>
+          <FooterServiceDiv>
+            <ServiceIcon>
+              <img src="./Assets/Icons/Locationicon.png" />
+            </ServiceIcon>
+            <ServiceInfo>
+              <h4>Our Active Location</h4>
+              <p>167 Airline Suite, 456GA3, UK</p>
+            </ServiceInfo>
+          </FooterServiceDiv>
+        </FooterServices>
+        <FooterFormSection>
+          <h4>Signup NewsLetter</h4>
+          <form action="/action_page.php" id="form1">
+            <input
+              placeholder="Enter Your Email"
+              type="text"
+              id="fname"
+              name="fname"
+            />
+            <button type="submit"></button>
+          </form>
+          <h4>Download Mobile</h4>
+        </FooterFormSection>
+      </FooterInnerContainer>
+    </FooterContainer>
   );
 }
+
 export default Footer;
 
-const Container = styled.div`
-  width: 100%;
-  height: 300px;
-  background-color: #363636;
-  color: white;
-  display: flex;
-  border-bottom: 1px solid grey;
-  padding-top: 50px;
+const FooterContainer = styled.footer`
+  background-color: #393232;
+  font-family: hindLight;
+  font-height: 0.9rem;
 `;
 
-const First = styled.div`
-  width: 40%;
-  margin-left: 100px;
-  margin-right: 20px;
-  padding: 15px;
-`;
-const FirstContainer = styled.div`
-  padding-top: 15px;
-`;
-
-const Second = styled.div`
-  width: 30%;
-  margin-left: 20px;
-`;
-const SecondContainer = styled.div`
-  display: flex;
-  padding: 10px;
-`;
-const Image = styled.div`
+const FooterInnerContainer = styled.div`
+  padding: 3rem 0;
+  width: 85%;
+  margin: 0 auto;
   display: flex;
   flex-direction: row;
-  padding-right: 10px;
+  justify-content: center;
+  align-item: center;
 `;
-const Column = styled.div`
+
+const FooterAboutUs = styled.div`
+  width: 33.33%;
+
+  > h3 {
+    padding: 0.5rem 0;
+    font-family: hindMedium;
+    color: #5ab9ea;
+  }
+  > p {
+    padding: 0.5rem 0;
+    color: #ffffff;
+  }
+`;
+
+const SocialLinks = styled.ul`
+  width: 50%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem 0;
+
+  > a > li {
+    display: flex;
+    justify-content: center;
+    align-item: center;
+    list-style-type: none;
+    width: 15px;
+    height: 15px;
+  }
+
+  > a > li > img {
+    width: 100%;
+  }
+`;
+
+const FooterServices = styled.div`
+  width: 33.33%;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
 `;
-const Third = styled.div`
-  width: 30%;
-  h4 {
-    padding: 15px;
+const FooterServiceDiv = styled.div`
+  margin: 0.8rem 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+const ServiceIcon = styled.div`
+  width: 15px;
+  height: 15px;
+
+  > img {
+    width: 100%;
   }
 `;
-const App = styled.div`
-  img {
-    width: 130px;
-    border-radius: 6px;
-    padding: 15px;
+const ServiceInfo = styled.div`
+  padding: 0 0.7rem;
+  color: #ffffff;
+
+  > h4 {
+    font-weight: 500;
+    font-size: 12px;
+  }
+
+  > p {
+    font-size: 11px;
+    font-weight: 200;
   }
 `;
-const Copyrights = styled.div`
-  background-color: #363636;
-  color: white;
-  padding: 15px;
-  p {
-    text-align: center;
+
+const FooterFormSection = styled.div`
+  width: 33.33%;
+  > form > input {
+    border: none;
+    padding: 10px;
+    border-radius: 5px 0px 0px 5px;
   }
 `;
