d38e1432947c0e4db134136186a5282ef6606ca2
SarkarPriyanshu <provashpriyanshu@gmail.com>
2021-03-15 12:58:22 +0530

    Footer Completed working on testimonial section

--- frontend/src/Components/Footer/footer.js
+++ frontend/src/Components/Footer/footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { BsArrowRightShort } from "react-icons/bs";
 
 function Footer() {
   return (
@@ -76,17 +77,33 @@ function Footer() {
               type="text"
               id="fname"
               name="fname"
+              autocomplete="off"
             />
-            <button type="submit"></button>
+            <button type="">
+              <BsArrowRightShort />
+            </button>
           </form>
           <h4>Download Mobile</h4>
         </FooterFormSection>
       </FooterInnerContainer>
+      <FooterCopyright>
+        <FooterCopyrightInnerContainer>
+          <p>
+            Copyrights © 2021. All Rights Reserved by <span>SEVA</span>
+          </p>
+        </FooterCopyrightInnerContainer>
+      </FooterCopyright>
     </FooterContainer>
   );
 }
 
 export default Footer;
+
+const media = {
+  desktop: "@media only screen and(min-width:1000px)",
+  tablet: "@media only screen and(min-width:768px)",
+  mobile: "@media only screen and(min-width:414px)",
+};
 
 const FooterContainer = styled.footer`
   background-color: #393232;
@@ -96,16 +113,16 @@ const FooterContainer = styled.footer`
 
 const FooterInnerContainer = styled.div`
   padding: 3rem 0;
-  width: 85%;
+  width: 90%;
   margin: 0 auto;
   display: flex;
   flex-direction: row;
-  justify-content: center;
+  justify-content: space-between;
   align-item: center;
 `;
 
 const FooterAboutUs = styled.div`
-  width: 33.33%;
+  width: 33%;
 
   > h3 {
     padding: 0.5rem 0;
@@ -113,6 +130,7 @@ const FooterAboutUs = styled.div`
     color: #5ab9ea;
   }
   > p {
+    font-size: 0.9rem;
     padding: 0.5rem 0;
     color: #ffffff;
   }
@@ -141,12 +159,13 @@ const SocialLinks = styled.ul`
 `;
 
 const FooterServices = styled.div`
-  width: 33.33%;
+  width: 33%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: flex-start;
 `;
+
 const FooterServiceDiv = styled.div`
   margin: 0.8rem 0;
   display: flex;
@@ -178,10 +197,54 @@ const ServiceInfo = styled.div`
 `;
 
 const FooterFormSection = styled.div`
-  width: 33.33%;
+  width: 33%;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+
+  > h4 {
+    font-weight: 400;
+  }
+  > form {
+    padding: 10px 0;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+  }
+
   > form > input {
     border: none;
     padding: 10px;
     border-radius: 5px 0px 0px 5px;
+  }
+  > form > button {
+    background-color: #5ab9ea;
+
+    padding: 9.6px;
+    border: none;
+    border-radius: 0px 5px 5px 0px;
+  }
+`;
+
+const FooterCopyright = styled.div`
+  border-top: 0.5px solid #5ab9ea;
+  padding: 1rem 0;
+`;
+
+const FooterCopyrightInnerContainer = styled.div`
+  width: 90%;
+  margin: 0 auto;
+
+  > p {
+    font-family: hindLight;
+    font-weight: 300;
+    color: #ffffff;
+    font-size: 0.8rem;
+
+    > span {
+      color: #5ab9ea;
+    }
   }
 `;
