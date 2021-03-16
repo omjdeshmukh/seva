846202c59bfd3d2774dd750f9c543df7016232c1
SarkarPriyanshu <provashpriyanshu@gmail.com>
2021-03-16 14:51:48 +0530

    Added login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,23 +7,9 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AfterLogin from "./Components/UsersPage/AfterLogin";
 import Footer from "./Components/Footer/footer";
 import ProviderDashboard from "./Components/ProviderDashboard/Dashboard";
-
 import AdminDashboard from "./Components/admin/AdminDashboard";
-
-//Set the Token from backend to sessionStorage of a browser
-function setUserToken(userToken) {
-  if (userToken === "") {
-    return;
-  }
-  sessionStorage.setItem("token", JSON.stringify(userToken));
-}
-
-//Check is there is any session token available in sessionStorage
-function getToken() {
-  const tokenString = sessionStorage.getItem("token");
-  const userToken = JSON.parse(tokenString);
-  return userToken ? true : false;
-}
+import Login from "./Components/Login_Signup/Login";
+import Signup from "./Components/Login_Signup/Signup";
 
 //Main App
 function App() {
@@ -31,25 +17,17 @@ function App() {
 
   // Rerender based on session Token
   useEffect(() => {
-    setUserToken(token ? token.data.token : "");
-  }, [token]);
+    // setUserToken(token ? token.data.token : "");
+  }, []);
 
   return (
     <Router>
       <div className="App">
         <Navbar setToken={setToken} />
         <Switch>
-          <Route
-            path="/"
-            exact
-            component={getToken() ? AfterLogin : Homepage}
-          />
-
-          <Route
-            path="/"
-            exact
-            component={getToken() ? AfterLogin : Homepage}
-          />
+          <Route path="/" exact component={Homepage} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/signup" exact component={Signup} />
           <Route path="/admin" exact component={AdminDashboard} />
           <Route path="/provider" exact component={ProviderDashboard} />
         </Switch>
