345d0f940dafab77e2978ab7cabbf088faf66082
SarkarPriyanshu <provashpriyanshu@gmail.com>
2021-03-17 17:11:24 +0530

    Added dashboard

--- frontend/src/Components/admin/Dashboard/adminDashboard.js
+++ frontend/src/Components/admin/Dashboard/adminDashboard.js
@@ -1,159 +1,114 @@
-import React from "react";
-import {Form ,Button , Card} from 'react-bootstrap';
-import { Container, Row, Col } from 'reactstrap';
+import React, { useState, useEffect } from "react";
+import { Form, Button, Card } from "react-bootstrap";
+import { Container, Row, Col } from "reactstrap";
 import styled from "styled-components";
 import axios from "axios";
 
 function Dashboard(props) {
+  const [user, setUser] = useState();
+  const [category, setCategory] = useState();
+  const [totalServices, setTotalServices] = useState();
+  const [totalSuggestion, setTotalSuggestion] = useState();
 
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/admin/category")
+      .then((response) => setCategory(response.data))
+      .catch((err) => console.log(err.message));
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/service")
+      .then((response) => setTotalServices(response.data))
+      .catch((err) => console.log(err.message));
+  }, []);
 
   return (
     <>
-
-    <Container>
-      <Row xs="2">
-          <Col>
-          <CardContainer>
-         <CardInnerContainer>
-           <InfoContainer>
-             <img src="https://i.pinimg.com/474x/95/d1/17/95d11754e7ec8e94cbdbed88cd67668a.jpg" alt="" />
-             <Info>
-             <h4>
-                    4
-               </h4>
-               <h5>Total Category</h5>
-            
-               
-             </Info>
-             <Action>
-             {/* <Button color="danger" onClick={ ActionDelete }>Delete</Button> */}
-             </Action>
-            
-           </InfoContainer>
-           
-         </CardInnerContainer>
-       </CardContainer>
-
-          </Col>
-          <Col>
-          <CardContainer>
-         <CardInnerContainer>
-           <InfoContainer>
-           <img src="http://cdn.onlinewebfonts.com/svg/img_281676.png" alt="" />
-             <Info>
-                <h4>
-                    5
-               </h4>
-               <h5>Total Suggestion</h5>
-               
-             </Info>
-             <Action>
-             {/* <Button color="danger" onClick={ ActionDelete }>Delete</Button> */}
-             </Action>
-           </InfoContainer>
-         </CardInnerContainer>
-       </CardContainer>
-
-          </Col>
-          <Col>
-          <CardContainer>
-         <CardInnerContainer>
-         <InfoContainer>
-           <img src="https://cdn2.iconfinder.com/data/icons/font-awesome/1792/user-512.png" alt="" />
-             <Info>
-             <h4>
-                    10 +
-               </h4>
-               <h5>Total User</h5>
-               
-             </Info>
-             <Action>
-             {/* <Button color="danger" onClick={ ActionDelete }>Delete</Button> */}
-             </Action>
-           </InfoContainer>
-         </CardInnerContainer>
-       </CardContainer>
-          </Col>
-
-          <Col>
-          <CardContainer>
-         <CardInnerContainer>
-         <InfoContainer>
-           <img src="https://static.thenounproject.com/png/684821-200.png" alt="" />
-             <Info>
-               <h4>
-                    20 + 
-               </h4>
-               <h5>Total Service</h5>
-               
-             </Info>
-             <Action>
-             {/* <Button color="danger" onClick={ ActionDelete }>Delete</Button> */}
-             </Action>
-           </InfoContainer>
-         </CardInnerContainer>
-       </CardContainer>
-          </Col>
-      
-
-      </Row>
-    </Container>
-       
+      <DashBoardContainer>
+        <DashBoardInnerContainer>
+          <CatagorySection>
+            <img src="" alt="" />
+            <InfoSection>
+              <p>Number</p>
+              <small>Total Category</small>
+            </InfoSection>
+          </CatagorySection>
+          <SuggectionSection>
+            <img src="" alt="" />
+            <InfoSection>
+              <p>Number</p>
+              <small>Total Suggestion</small>
+            </InfoSection>
+          </SuggectionSection>
+          <UserSection>
+            <img src="" alt="" />
+            <InfoSection>
+              <p>Number</p>
+              <small>Total Users</small>
+            </InfoSection>
+          </UserSection>
+          <TotalServices>
+            <img src="" alt="" />
+            <InfoSection>
+              <p>Number</p>
+              <small>Total Services</small>
+            </InfoSection>
+          </TotalServices>
+        </DashBoardInnerContainer>
+      </DashBoardContainer>
     </>
   );
 }
 
 export default Dashboard;
 
-
-
-const CardContainer = styled.div`
-margin: 1rem 0;
-padding: 2rem 0;
-
-width: 100%;
-box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
-border-radius: 10px;
-
-// font-family: hindLight;
-`;
-const CardInnerContainer = styled.div`
-width: 95%;
-margin: 0 auto;
-padding: 2rem 0;
-
+const DashBoardContainer = styled.div`
+  border: 1px solid #ccc;
+  font-family: hindLight;
 `;
 
-const InfoContainer = styled.div`
-display: flex;
-flex-direction: row;
-justify-content: flex-start;
-// align-items: center;
-> img {
-  width: 4rem;
-  height: 4rem;
-  border: none;
-  border-radius: 10%;
-}
+const DashBoardInnerContainer = styled.div`
+  width: 95%;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  > div {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    min-width: 250px;
+    min-height: 150px;
+    border-radius: 10px;
+    padding: 1rem;
+    margin: 0.5rem;
+    display: flex;
+    flex-direction: row;
+    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+
+    > img {
+      width: 60px;
+      height: 60px;
+    }
+  }
 `;
-const Info = styled.div`
-padding: 0 1rem;
-text-align: left;
-width: 85%;
-color:black;
-> h4 {
-  padding: 0;
-}
+
+const CatagorySection = styled.div``;
+const SuggectionSection = styled.div``;
+const UserSection = styled.div``;
+const TotalServices = styled.div``;
+const InfoSection = styled.div`
+  padding: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: left;
+  line-height: 0.5rem;
 `;
-
-const Action = styled.div`
-padding: 0 1rem;
-align: right;
-> button {
-  padding: 12;
-}
-`;
-
-
-
-
