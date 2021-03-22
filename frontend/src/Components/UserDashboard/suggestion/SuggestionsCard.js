b5cbb5342078156a862ce46d284433453b383ab9
bharath <bharathraj693@gmail.com>
2021-03-22 23:46:05 +0530

    Suggestions update page is completed

--- frontend/src/Components/UserDashboard/suggestion/SuggestionsCard.js
+++ frontend/src/Components/UserDashboard/suggestion/SuggestionsCard.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 // import {Button} from 'reactstrap'
 import axios from 'axios'
 // import { Input} from 'reactstrap'
-import { Modal} from 'react-bootstrap'
+import { Modal } from 'react-bootstrap'
 import {
   Button,
   Form,
@@ -13,147 +13,69 @@ import {
   FormText,
   FormFeedback,
 } from "reactstrap";
-function SuggestionCard(props){
-  // const {ServiceType, category, ServiceDescription} =props.suggestions
+import UpdateSuggestions from './UpdateSuggestions'
+function SuggestionCard(props) {
+  const[open , setOpen] =useState(false)
+  const { servicetype, categoryName, servicedescription ,suggestionid} = props
   // console.log(ServiceType)
 
-  const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  
 
-  const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwNGNjMWZkNDNlODI4MjJhODU3ODZlNiIsImlhdCI6MTYxNTk5MDc1NH0.fjlkDknRnl1MBC2gJMLFRpo4pZdQJADO5DGe3OGY1oA"
-  function ActionDelete(){
-      axios('http://localhost:5000/user/suggestion/'+`${props.suggestionid}`,{
-        method:"DELETE",
-        headers:{
-           "content-type":"application/json",
-           "auth-token":`${token}`
-        }
-      })
-      .then((data) =>{console.log(data)
-      alert("user suggestion deleted succesfully")
-      refreshPage()
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwNGNjMWZkNDNlODI4MjJhODU3ODZlNiIsImlhdCI6MTYxNTk5MDc1NH0.fjlkDknRnl1MBC2gJMLFRpo4pZdQJADO5DGe3OGY1oA"
+  function ActionDelete() {
+    axios('http://localhost:5000/user/suggestion/' + `${suggestionid}`, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json",
+        "auth-token": `${token}`
+      }
     })
-      .catch((err) =>{console.log(err)
-        alert("someting went wrong !!!"+ err.message)
+      .then((data) => {
+        console.log(data)
+        alert("user suggestion deleted succesfully")
+        refreshPage()
+      })
+      .catch((err) => {
+        console.log(err)
+        alert("someting went wrong !!!" + err.message)
       })
   }
 
-      
-
-  function refreshPage(){
+  function refreshPage() {
     window.location.reload()
   }
+
   
-    return(
-           <>
+
+  return (
+    <>
       <CardContainer>
-         <CardInnerContainer>
-           <InfoContainer>
-             <Info>
-              <h3>{props.servicetype}</h3>
-              <small>{props.categoryName}</small>
+        <CardInnerContainer>
+          <InfoContainer>
+            <Info>
+              <h3>{servicetype}</h3>
+              {<small>{categoryName}</small>}
               <br></br>
-              <small>{props.servicedescription}</small>
-             </Info>
-           </InfoContainer>
-
-           <ButtonContainer>
-           <Action>
-             <Button color="danger" onClick={ ActionDelete }>Delete</Button>
-             </Action>
-             <Action>
-             <Button color="primary" onClick={handleShow} >Update
+              <small>{servicedescription}</small>
+            </Info>
+          </InfoContainer>
+          <ButtonContainer>
+            <Action>
+              <Button color="danger" onClick={ActionDelete}>Delete</Button>
+            </Action>
+            <Action>
+              <Button color="primary" onClick={() =>setOpen(true)} >Update
              </Button>
-
-
-             </Action>
-           </ButtonContainer>
-         </CardInnerContainer>
-       </CardContainer>
-
-       <Modal show={show} onHide={handleClose} animation={false}>
-        <Modal.Header closeButton>
-          <Modal.Title>Update Suggestion- {props.suggestionid}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-        <Form onSubmit={handleClose}>
-            <FormGroup>
-              <Label for="ServiceType">Service Type</Label>
-              <Input
-                type="text"
-                placeholder="ServiceType"
-                // value={FormData.ServiceType || ""}
-                // name="ServiceType"
-                // onChange={handleInput}
-              />
-            </FormGroup>
-
-            {/* <FormGroup>
-              <Label for="exampleSelect">Service Category</Label>
-              <Input
-                type="select"
-                name="category"
-                value={FormData.category}
-                onChange={handleInput}
-                id="exampleSelect"
-                defaultValue
-              >
-                <option>select category</option>
-
-                {category &&
-                  category.map((item, index) => {
-                    // console.log(item)
-                    // {console.log(item.category)}
-                    return (
-                      <>
-                        <option value={item._id}>{item.category}</option>
-                      </>
-                    );
-                  })}
-              </Input>
-            </FormGroup> */}
-
-            <FormGroup>
-              <Label for="ServicePinCode">Service Pincode</Label>
-              <Input
-                type="text"
-                placeholder="ServicePinCode"
-                // value={FormData.ServicePinCode || ""}
-                // name="ServicePinCode"
-                // onChange={handleInput}
-              />
-            </FormGroup>
-            <FormGroup>
-              <label htmlFor="exampleFormControlTextarea1">
-                Service Description
-              </label>
-              <textarea
-                className="form-control"
-                id="exampleFormControlTextarea1"
-                // value={FormData.ServiceDescription}
-                // name="ServiceDescription"
-                // onChange={handleInput}
-                rows="4"
-              />
-            </FormGroup>
-            <Button color="primary" size="lg" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          {/* <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button> */}
-        </Modal.Footer>
-      </Modal>
+            </Action>
+          </ButtonContainer>
+        </CardInnerContainer>
+      </CardContainer>
+      <UpdateSuggestions open={open} 
+                         close={setOpen}
+                         id={suggestionid}/>
     </>
-    )
+  )
 }
 export default SuggestionCard
 
