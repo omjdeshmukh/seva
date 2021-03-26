5937987f03f8ca6f6c3407cd4d8048f691a6f150
SarkarPriyanshu <provashpriyanshu@gmail.com>
2021-03-26 16:37:26 +0530

    Minor bug fixed

--- frontend/src/Components/UserDashboard/feedback/showFeedback.js
+++ frontend/src/Components/UserDashboard/feedback/showFeedback.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import FeedbackCard from "./FeedbackCard";
 import { getCookieData } from "../../userData";
+import Vector from "./vector";
+import styled from "styled-components";
 
 function ShowFeedback({ token, userId, pincode, role }) {
   const [feedback, setFeedback] = useState();
@@ -22,17 +24,61 @@ function ShowFeedback({ token, userId, pincode, role }) {
   //console.log(feedback)
   return (
     <div>
-      {feedback &&
-        feedback.map((item, i) => {
-          return (
-            <FeedbackCard
-              key={item._id}
-              feedbackid={item._id}
-              feedbackinfo={feedback[i].feedback}
-            />
-          );
-        })}
+      {feedback && feedback.length != 0 ? (
+        <>
+          {feedback &&
+            feedback.map((item, i) => {
+              return (
+                <FeedbackCard
+                  key={item._id}
+                  feedbackid={item._id}
+                  feedbackinfo={feedback[i].feedback}
+                />
+              );
+            })}
+        </>
+      ) : (
+        <>
+          <NoServiceDiv>
+            <h6>Sorry! No feedback available yet.</h6>
+            <VectorContainer>
+              <Vector />
+            </VectorContainer>
+          </NoServiceDiv>
+        </>
+      )}
     </div>
   );
 }
 export default ShowFeedback;
+
+const NoServiceDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  > h6 > a {
+    font-family: hindMedium;
+    text-decoration: none;
+    color: #393232;
+    font-weight: 300;
+  }
+
+  h6 > span {
+    font-family: hindMedium;
+    text-decoration: none;
+    color: #5ab9ea;
+    font-weight: 300;
+  }
+`;
+
+const VectorContainer = styled.div`
+  width: 150px;
+  height: 150px;
+
+  > svg {
+    width: 100%;
+    height: 15rem;
+  }
+`;
--- frontend/src/Components/UserDashboard/suggestion/ShowSuggestion.js
+++ frontend/src/Components/UserDashboard/suggestion/ShowSuggestion.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { getCookieData } from "../../userData";
 import SuggestionCard from "./SuggestionsCard";
+import Vector from "./vector";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 function ShowSuggestion({ pincode, token, userId, role }) {
   const [suggestions, setSuggestions] = useState();
@@ -19,19 +22,63 @@ function ShowSuggestion({ pincode, token, userId, role }) {
 
   return (
     <div>
-      {suggestions &&
-        suggestions.map((item, i) => {
-          return (
-            <SuggestionCard
-              key={item._id}
-              suggestionid={suggestions[i]._id}
-              servicetype={suggestions[i].ServiceType}
-              categoryName={suggestions[i].category.category}
-              servicedescription={suggestions[i].ServiceDescription}
-            />
-          );
-        })}
+      {suggestions && suggestions.length != 0 ? (
+        <>
+          {suggestions &&
+            suggestions.map((item, i) => {
+              return (
+                <SuggestionCard
+                  key={item._id}
+                  suggestionid={suggestions[i]._id}
+                  servicetype={suggestions[i].ServiceType}
+                  categoryName={suggestions[i].category.category}
+                  servicedescription={suggestions[i].ServiceDescription}
+                />
+              );
+            })}
+        </>
+      ) : (
+        <>
+          <NoServiceDiv>
+            <h6>Sorry! No Suggestions available yet.</h6>
+            <VectorContainer>
+              <Vector />
+            </VectorContainer>
+          </NoServiceDiv>
+        </>
+      )}
     </div>
   );
 }
 export default ShowSuggestion;
+
+const NoServiceDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  > h6 > a {
+    font-family: hindMedium;
+    text-decoration: none;
+    color: #393232;
+    font-weight: 300;
+  }
+
+  h6 > span {
+    font-family: hindMedium;
+    text-decoration: none;
+    color: #5ab9ea;
+    font-weight: 300;
+  }
+`;
+
+const VectorContainer = styled.div`
+  width: 150px;
+  height: 150px;
+
+  > svg {
+    width: 100%;
+    height: 15rem;
+  }
+`;
